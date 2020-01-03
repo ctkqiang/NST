61462d41c228849dfc12278b2dd673398d66c267
John Melody Me <johnmelodyme@icloud.com>
2020-01-03 17:01:03 +0800

    업데이트 -- John

--- app/src/main/java/com/starlabbioscience/nst/FLASH.java
+++ app/src/main/java/com/starlabbioscience/nst/FLASH.java
@@ -2,13 +2,35 @@ package com.starlabbioscience.nst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * @AUTHOR : JOHN MELODY ME
+ * @COPYRIGHT : BRAINTECH SDN BHD || STARLABS BIOSCIENCE SDN BHD
+ * @PROJECT: NST
+ * @DATE_STARTED: 2 JANUARY 2020
+ */
 
 public class FLASH extends AppCompatActivity {
+
+    Handler HANDLER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash);
+
+        HANDLER = new Handler();
+
+        HANDLER.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent TOMAIN;
+                TOMAIN = new Intent(FLASH.this, MUSICPLAYER.class);
+                startActivity(TOMAIN);
+            }
+        }, 4010);
     }
 }
--- app/src/main/java/com/starlabbioscience/nst/MUSICPLAYER.java
+++ app/src/main/java/com/starlabbioscience/nst/MUSICPLAYER.java
@@ -1,8 +1,13 @@
 package com.starlabbioscience.nst;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * @AUTHOR : JOHN MELODY ME
@@ -12,23 +17,61 @@ import android.view.View;
  */
 public class MUSICPLAYER extends AppCompatActivity {
 
+    MediaPlayer NST;
+    Button ATTENTION, MEMORY;
+
     @Override
     public void onStart(){
         super.onStart();
-    }
-
-    private void INIT(){
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        INIT();
+
+        ATTENTION = findViewById(R.id.Attention);
+        MEMORY = findViewById(R.id.Memory);
     }
 
     public void onClick(View view){
+        switch(view.getId()){
+            case R.id.Attention:
+                NST = MediaPlayer.create(MUSICPLAYER.this, R.raw.gta);
+                NST.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                NST.setVolume(0b1010000, 0b1010000);
+                NST.setLooping(false);
+                if (NST.isPlaying()){
+                    NST.pause();
+                    ATTENTION.setBackgroundResource(R.mipmap.atention);
+                    ATTENTION.setBackgroundResource(R.mipmap.attentionplay);
+                } else {
+                    NST.start();
+                    ATTENTION.setBackgroundResource(R.mipmap.attentionplay);
+                    ATTENTION.setBackgroundResource(R.mipmap.atention);
+                }
+                break;
 
+            case R.id.Memory:
+                NST = MediaPlayer.create(MUSICPLAYER.this, R.raw.gta);
+                NST.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                NST.setVolume(0b1010000, 0b1010000);
+                NST.setLooping(false);
+                if (NST.isPlaying()){
+                    NST.pause();
+                    MEMORY.setBackgroundResource(R.mipmap.mmemory);
+                    MEMORY.setBackgroundResource(R.mipmap.mmemory_playingime);
+                } else {
+                    NST.start();
+                    MEMORY.setBackgroundResource(R.mipmap.mmemory_playingime);
+                    for (int loop = 0b0; loop < 0b101; loop++){
+                        System.out.println("---");
+                    }
+                    MEMORY.setBackgroundResource(R.mipmap.mmemory_playingime);
+                    MEMORY.setBackgroundResource(R.mipmap.mmemory);
+
+                }
+                break;
+        }
     }
 }
